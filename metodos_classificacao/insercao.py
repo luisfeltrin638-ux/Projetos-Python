62613cf3192fad4590acd319386c090bff97f340
luisfeltrin638-ux <luis.feltrin.638@gmail.com>
2026-05-23 22:23:13 +0900

    Substituição da variável "decremento" para "j"

--- metodos_classificacao/insercao.py
+++ metodos_classificacao/insercao.py
@@ -1,12 +1,14 @@
+# Não adaptado para classificar strings ou retornar a lista em classficação invertida
 def insercao(lista):
     valor_atual = 0
+    
     for i in range(1, len(lista)):
         valor_atual = lista[i]
-        decremento = i - 1
+        j = i - 1
         
-        while decremento >= 0 and lista[decremento] > valor_atual:
-            lista[decremento + 1] = lista[decremento]
-            decremento -= 1
-        lista[decremento + 1] = valor_atual
+        while j >= 0 and lista[j] > valor_atual:
+            lista[j + 1] = lista[j]
+            j -= 1
+        lista[j + 1] = valor_atual
         
     return lista
--- metodos_classificacao/selecao.py
+++ metodos_classificacao/selecao.py
@@ -1,2 +1,17 @@
+# Não adaptado para classificar adequadamente strings ou retornar a lista em classficação invertida
 def selecao(lista):
     
+    for i in range(len(lista)):
+        j = i + 1
+
+        while j < len(lista):
+
+            if lista[i] > lista[j]:
+                
+                menor = lista[j]
+
+                lista[j] = lista[i]
+                lista[i] = menor
+            j += 1
+
+    return lista
